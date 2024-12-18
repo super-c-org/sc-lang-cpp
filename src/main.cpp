e956893d73645a82786159b494de43ac8fda7313
Dali Wang <dali.wang@gmail.com>
2024-12-18 12:00:39 -0600

    test llvm initialization

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,15 @@
 
+#include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/raw_ostream.h"
+
+namespace scl {
+
+
+} //namespace scl
+
 auto main(int argc, char* argv[]) -> int
 {
+    llvm::InitLLVM init_llvm(argc, argv);
+    llvm::outs() << "Hello, this is super-C compiler\n"; 
     return 0;
 }
