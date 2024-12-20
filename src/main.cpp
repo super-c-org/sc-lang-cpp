2848b87e62826fec998b4aa42ebba2e532341340
Dali Wang <dali.wang@gmail.com>
2024-12-19 20:08:24 -0600

    test llvm commandline library

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,13 @@ auto main(int argc, char* argv[]) -> int
     llvm::outs() << "Hello, this is super-C compiler\n";
 
     namespace cl = llvm::cl;
+    cl::opt<std::string> OutputFilename("o", cl::desc("Specify output filename"), cl::value_desc("filename"));
+    cl::opt<std::string> InputFilename(cl::Positional, cl::desc("<input file>"), cl::Required);
+
     cl::ParseCommandLineOptions(argc, argv);
-    cl::opt<std::string> input_file_name(cl::Positional, cl::desc("<input file>"), cl::Required);
+
+    printf("%s \n", InputFilename.c_str());
+    printf("%s \n", OutputFilename.c_str());
 
     return 0;
 }
