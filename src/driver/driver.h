83036cfc4365ac80f1eafdc4de967d26b73eb1dd
Dali Wang <dali.wang@gmail.com>
2025-01-01 15:13:51 -0600

    refactor to google style

--- src/driver/driver.h
+++ src/driver/driver.h
@@ -1,5 +1,5 @@
 #pragma once
 
 namespace scl {
-auto runCompiler() -> void;
-} // namespace scl
+auto RunCompiler() -> void;
+}  // namespace scl
--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,15 @@ auto main(int argc, char* argv[]) -> int {
     llvm::outs() << "Hello, this is super-C compiler\n";
 
     namespace cl = llvm::cl;
-    cl::opt<std::string> OutputFilename(
+    cl::opt<std::string> output_filename(
         "o", cl::desc("Specify output filename"), cl::value_desc("filename"));
-    cl::opt<std::string> InputFilename(cl::Positional, cl::desc("<input file>"),
-                                       cl::Required);
+    cl::opt<std::string> input_filename(cl::Positional,
+                                        cl::desc("<input file>"), cl::Required);
 
     cl::ParseCommandLineOptions(argc, argv);
 
-    printf("%s \n", InputFilename.c_str());
-    printf("%s \n", OutputFilename.c_str());
+    printf("%s \n", input_filename.c_str());
+    printf("%s \n", output_filename.c_str());
 
     return 0;
 }
